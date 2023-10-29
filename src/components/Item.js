c8e726be5d8fc0656bbf30159075dde28e98ccaa
John <johnnkonge2020@gmail.com>
2023-10-29 07:15:19 +0300

    atleast i tried

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import items from "../data/items";
 
 function Item({ name, category }) {
+  const [item,setItem] = useState(true) 
+  
+
+  function handleAddItem(){
+
+    
+    console.log('clicked')
+    const newItem = Item();
+    const newItemArr = [...items, newItem]
+    setItem(newItemArr)
+  }
+ 
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAddItem}>Add to Cart</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [filterBy, setfilterBy] = useState('All')
+  const[item,setItem] = useState(items)
+  const itemDisplay = items.filter((item)=>{
+    if(filterBy === 'All'){
+      return true
+    }else {
+      return item.id === filterBy
+    }
+  })
+
+
+    function handleFilter(event){
+      setfilterBy(event.target.value);
+    }
+  const itemList = itemDisplay.map((item)=>(
+    <li key={item.id} onClick={handleFilter}>
+      {item.name}
+    </li>
+  ))
+
   return (
-    <div className="ShoppingList">
+    <div className="ShoppingList" onChange={handleFilter}>
       <div className="Filter">
         <select name="filter">
           <option value="All">Filter by category</option>
